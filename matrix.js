e60f5b3c944f4ea392f1d0907a4ff369c5f55b71
ben-southpaw <ben.dryden@gmail.com>
2016-07-20 14:55:52 +1200

    ideas for getMatrix

--- matrix.js
+++ matrix.js
@@ -1,4 +1,12 @@
+module.exports = {
+  getMatrix:getMatrix,
+  updateMatrix: updateMatrix
+  }
+
+
 function getMatrix (n) {
+  arr = Array(n).fill(0, 0, n)
+  return arr
 }
 
 function updateMatrix (matrix, coords, value) {
